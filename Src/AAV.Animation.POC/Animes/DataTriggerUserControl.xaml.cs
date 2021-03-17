5735b9f4ba8bea8132f53a67bcb5524d7da5bdd6
AlexPi <alex.pigida@outlook.com>
2021-03-16 20:59:03 -0400

    port to Net5 + Color animated style by DataTrigger !!!

--- Src/AAV.Animation.POC/Animes/DataTriggerUserControl.xaml.cs
+++ Src/AAV.Animation.POC/Animes/DataTriggerUserControl.xaml.cs
@@ -3,16 +3,13 @@ using System.Windows.Controls;
 
 namespace AAV.Animation.POC
 {
-    public partial class DataTriggerUserControl : UserControl
+  public partial class DataTriggerUserControl : UserControl
+  {
+    public DataTriggerUserControl()
     {
-        public DataTriggerUserControl()
-        {
-            InitializeComponent();
-            DataContext = this;
-        }
-
-        public static readonly DependencyProperty IsCurrentProperty = DependencyProperty.Register("IsCurrent", typeof(bool), typeof(DataTriggerUserControl), new PropertyMetadata(true)); public bool IsCurrent { get { return (bool)GetValue(IsCurrentProperty); } set { SetValue(IsCurrentProperty, value); } }
-
-
+      InitializeComponent();
+      DataContext = this;
     }
+    public static readonly DependencyProperty IsCurrentProperty = DependencyProperty.Register("IsCurrent", typeof(bool), typeof(DataTriggerUserControl), new PropertyMetadata(true)); public bool IsCurrent { get { return (bool)GetValue(IsCurrentProperty); } set { SetValue(IsCurrentProperty, value); } }
+  }
 }
---
+++ Src/AAV.Animation.POC/Vws/DataTriggerAnimatedStyle.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AAV.Animation.POC.Vws
+{
+  public partial class DataTriggerAnimatedStyle : Window
+  {
+    public DataTriggerAnimatedStyle()
+    {
+      InitializeComponent();
+      DataContext = this;
+    }
+
+    public static readonly DependencyProperty IsCurrentProperty = DependencyProperty.Register("IsCurrent", typeof(bool), typeof(DataTriggerAnimatedStyle), new PropertyMetadata(true)); public bool IsCurrent { get { return (bool)GetValue(IsCurrentProperty); } set { SetValue(IsCurrentProperty, value); } }
+
+    void Button_Click(object sender, RoutedEventArgs e)    {      Close();    }
+  }
+}
