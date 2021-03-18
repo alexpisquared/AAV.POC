25218d653f2caceee25d43869fac5b7324c045b9
AlexPi <alex.pigida@outlook.com>
2021-03-17 20:52:32 -0400

    progress++

--- Src/AAV.Animation.POC/Vws/DataTriggerAnimatedStyle.xaml.cs
+++ Src/AAV.Animation.POC/Vws/DataTriggerAnimatedStyle.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace AAV.Animation.POC.Vws
 {
@@ -22,29 +11,16 @@ namespace AAV.Animation.POC.Vws
       InitializeComponent();
       DataContext = this;
     }
+    public static readonly DependencyProperty IsCurrentProperty = DependencyProperty.Register("IsCurrent", typeof(bool), typeof(DataTriggerAnimatedStyle), new PropertyMetadata(true)); public bool IsCurrent { get => (bool)GetValue(IsCurrentProperty); set => SetValue(IsCurrentProperty, value); }
 
-    public static readonly DependencyProperty IsCurrentProperty = DependencyProperty.Register("IsCurrent", typeof(bool), typeof(DataTriggerAnimatedStyle), new PropertyMetadata(true)); public bool IsCurrent { get { return (bool)GetValue(IsCurrentProperty); } set { SetValue(IsCurrentProperty, value); } }
-
-    void onChange(object s, RoutedEventArgs e)
-    {
-      foreach (var item in ((Cities.Models.SampleListItem[])dg2.ItemsSource))
-      {
-        item.Name = item.Name == "Mauris" ? "Abc123" : "Mauris";
-      }
-
-      new MainWindow().Show();
-    }
-    void Button_Click(object sender, RoutedEventArgs e) { Close(); }
+    void onChange(object s, RoutedEventArgs e) { foreach (var r in ((Cities.Models.SampleListItem[])dg2.ItemsSource)) r.Name = r.Name == "Mauris" ? "Abc123" : "Mauris"; }
+    void onShowMainWindow(object s, RoutedEventArgs e) => new MainWindow().Show();
+    void Button_Click(object sender, RoutedEventArgs e) => Close();
   }
 }
 
 namespace Cities.Models
 {
-  public class City
-  {
-    public string Name { get; set; }
-    public string Country { get; set; }
-  }
   public class SampleListItem : INotifyPropertyChanged
   {
     public event PropertyChangedEventHandler PropertyChanged;
