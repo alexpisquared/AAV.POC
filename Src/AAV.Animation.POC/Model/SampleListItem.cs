b68b1031756049688f7db57ce9f1cd6fb91811dc
AlexPi <alex.pigida@outlook.com>
2021-03-19 17:10:08 -0400

    more cleanup

---
+++ Src/AAV.Animation.POC/Model/SampleListItem.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace AAV.Animation.POC.Model
+{
+  public class SampleListItem : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+      if (PropertyChanged != null)
+      {
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+      }
+    }
+
+    double _ID = 0; public double ID
+    {
+      get => _ID;
+
+      set
+      {
+        if (_ID != value)
+        {
+          _ID = value;
+          OnPropertyChanged("ID");
+        }
+      }
+    }
+    string _Name = string.Empty; public string Name
+    {
+      get => _Name;
+
+      set
+      {
+        if (_Name != value)
+        {
+          _Name = value;
+          OnPropertyChanged("Name");
+        }
+      }
+    }
+    bool _Done = false; public bool Done
+    {
+      get => _Done;
+
+      set
+      {
+        if (_Done != value)
+        {
+          _Done = value;
+          OnPropertyChanged("Done");
+        }
+      }
+    }
+    ImageSource _Look = null; public ImageSource Look
+    {
+      get => _Look;
+
+      set
+      {
+        if (_Look != value)
+        {
+          _Look = value;
+          OnPropertyChanged("Look");
+        }
+      }
+    }
+  }
+}
--- Src/AAV.Animation.POC/Vws/DataTriggerAnimatedStyle.xaml.cs
+++ Src/AAV.Animation.POC/Vws/DataTriggerAnimatedStyle.xaml.cs
@@ -13,76 +13,8 @@ namespace AAV.Animation.POC.Vws
     }
     public static readonly DependencyProperty IsCurrentProperty = DependencyProperty.Register("IsCurrent", typeof(bool), typeof(DataTriggerAnimatedStyle), new PropertyMetadata(true)); public bool IsCurrent { get => (bool)GetValue(IsCurrentProperty); set => SetValue(IsCurrentProperty, value); }
 
-    void onChange(object s, RoutedEventArgs e) { foreach (var r in ((Cities.Models.SampleListItem[])dg2.ItemsSource)) r.Name = r.Name == "Mauris" ? "Abc123" : "Mauris"; }
+    void onChange(object s, RoutedEventArgs e) { foreach (var r in ((AAV.Animation.POC.Model.SampleListItem[])dg2.ItemsSource)) r.Name = r.Name == "Mauris" ? "Abc123" : "Mauris"; }
     void onShowMainWindow(object s, RoutedEventArgs e) => new MainWindow().Show();
     void onClose(object s, RoutedEventArgs e) => Close();
-  }
-}
-
-namespace Cities.Models
-{
-  public class SampleListItem : INotifyPropertyChanged
-  {
-    public event PropertyChangedEventHandler PropertyChanged;
-    protected virtual void OnPropertyChanged(string propertyName)
-    {
-      if (PropertyChanged != null)
-      {
-        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-      }
-    }
-
-    double _ID = 0; public double ID
-    {
-      get => _ID;
-
-      set
-      {
-        if (_ID != value)
-        {
-          _ID = value;
-          OnPropertyChanged("ID");
-        }
-      }
-    }
-    string _Name = string.Empty; public string Name
-    {
-      get => _Name;
-
-      set
-      {
-        if (_Name != value)
-        {
-          _Name = value;
-          OnPropertyChanged("Name");
-        }
-      }
-    }
-    bool _Done = false; public bool Done
-    {
-      get => _Done;
-
-      set
-      {
-        if (_Done != value)
-        {
-          _Done = value;
-          OnPropertyChanged("Done");
-        }
-      }
-    }
-    ImageSource _Look = null; public ImageSource Look
-    {
-      get => _Look;
-
-      set
-      {
-        if (_Look != value)
-        {
-          _Look = value;
-          OnPropertyChanged("Look");
-        }
-      }
-    }
   }
 }
