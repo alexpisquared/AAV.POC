719e5f6e02646d5b169b98449d368194eeea51b3
AlexPi <alex.pigida@outlook.com>
2021-03-19 16:23:30 -0400

    ++

--- Src/AAV.Animation.POC/Animes/GradientWave.xaml.cs
+++ Src/AAV.Animation.POC/Animes/GradientWave.xaml.cs
@@ -6,6 +6,12 @@ namespace AAV.Animation.POC.Animes
   public partial class GradientWave : UserControl
   {
     public GradientWave() => InitializeComponent();
-    void onChange(object s, RoutedEventArgs e) { foreach (var r in ((Cities.Models.SampleListItem[])dg2.ItemsSource)) r.Name = r.Name == "Mauris" ? "Abc123" : "Mauris"; }
+
+    //void onChange(object s, RoutedEventArgs e) { foreach (var r in ((Cities.Models.SampleListItem[])dg2.ItemsSource)) r.Name = r.Name == "Mauris" ? "Abc123" : "Mauris"; }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
   }
 }
