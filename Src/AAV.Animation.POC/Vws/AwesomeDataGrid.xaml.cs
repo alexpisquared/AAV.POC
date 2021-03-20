7458a0e634df2ddc01497da7d82350241b923918
AlexPi <alex.pigida@outlook.com>
2021-03-20 18:22:25 -0400

    Update AwesomeDataGrid.xaml.cs

--- Src/AAV.Animation.POC/Vws/AwesomeDataGrid.xaml.cs
+++ Src/AAV.Animation.POC/Vws/AwesomeDataGrid.xaml.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace AAV.Animation.POC.Vws
 {
-    public partial class AwesomeDataGrid : Window
+  public partial class AwesomeDataGrid : Window
+  {
+    public AwesomeDataGrid()
     {
-        public AwesomeDataGrid()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
+
+      if (Environment.MachineName == "RAZER1") { Top = 1700; Left = 1100; }
+    }
     void onClose(object s, RoutedEventArgs e) => Close();
   }
 }
