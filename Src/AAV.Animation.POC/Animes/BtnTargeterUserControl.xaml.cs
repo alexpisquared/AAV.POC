ae8a6fdca441861ecb1be0f6bd7842a39133ae84
AlexPi <alex.pigida@outlook.com>
2022-06-01 16:45:15 -0400

    visual button clicker/presser

---
+++ Src/AAV.Animation.POC/Animes/BtnTargeterUserControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace AAV.Animation.POC.Animes;
+
+public partial class BtnTargeterUserControl : UserControl
+{
+  public BtnTargeterUserControl()
+  {
+    InitializeComponent();
+  }
+}
